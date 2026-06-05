ce81fbb07bcbefaf3fac46c42c9e0c7af8f64aed
Krishna S <265krishna77@gmail.com>
2026-06-05 15:18:16 +0530

    Update config.h

--- task3/include/config.h
+++ task3/include/config.h
@@ -1,1 +1,15 @@
+#ifndef CONFIG_H
+#define CONFIG_H
 
+// Irrigation duration
+#define IRRIGATION_SECONDS 5
+
+// Pin assignments
+const uint8_t LED_STATUS = 13;
+const uint8_t BTN_MANUAL = 2;
+const uint8_t RELAY_VALVE = 8;
+
+// Debounce timing
+const unsigned long DEBOUNCE_MS = 50;
+
+#endif
